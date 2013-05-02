a9ebb217d885d240e3f72dd04a409e9e897b7028
roglew <rglew56@gmail.com>
2013-05-02 00:15:56 -0400

    Initial commit

---
+++ ViRepeatMacro.py
@@ -0,0 +1,14 @@
+import sublime_plugin
+
+
+class ViRepeatLastMacro(sublime_plugin.TextCommand):
+	def run(self, edit):
+		i = 0
+		history = self.view.command_history(i, True)
+		while (history[0] != 'vi_replay_macro' and history[0] is not None):
+			history = self.view.command_history(i)
+			i -= 1
+
+		if history[0] is not None:
+			char = history[1]["character"]
+			self.view.run_command("vi_replay_macro", {"character": char})
